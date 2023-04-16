652b87c66c6eddaca0fbcf4b167d72dfe0cb77fd
Arvind Kasale <arvind.kasale@appdirect.com>
2023-04-16 15:36:55 +0530

    Add designation repository

--- user-management/persistence/src/main/java/com/intuit/feedback/user/mgmt/persistence/repository/BusinessUnitMongoRepository.java
+++ user-management/persistence/src/main/java/com/intuit/feedback/user/mgmt/persistence/repository/BusinessUnitMongoRepository.java
@@ -3,7 +3,6 @@ package com.intuit.feedback.user.mgmt.persistence.repository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.intuit.feedback.user.mgmt.persistence.entity.user.BusinessUnit;
-import com.intuit.feedback.user.mgmt.persistence.entity.user.Designation;
 import reactor.core.publisher.Mono;
 
 public interface BusinessUnitMongoRepository extends ReactiveMongoRepository<BusinessUnit, String> {
--- user-management/persistence/src/main/java/com/intuit/feedback/user/mgmt/persistence/repository/DesignationMongoRepository.java
+++ user-management/persistence/src/main/java/com/intuit/feedback/user/mgmt/persistence/repository/DesignationMongoRepository.java
@@ -3,6 +3,9 @@ package com.intuit.feedback.user.mgmt.persistence.repository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.intuit.feedback.user.mgmt.persistence.entity.user.Designation;
+import reactor.core.publisher.Mono;
 
 public interface DesignationMongoRepository extends ReactiveMongoRepository<Designation, String> {
+
+	Mono<Designation> findFirstByCode(String code);
 }
