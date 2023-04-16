0b26603fb20b55aa4d5f6152249f1fec208759fe
Arvind Kasale <arvind.kasale@appdirect.com>
2023-04-16 11:34:34 +0530

    Add user-management structure

---
+++ user-management/src/test/java/com/intuit/feedback/user/mgmt/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.feedback.user.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
